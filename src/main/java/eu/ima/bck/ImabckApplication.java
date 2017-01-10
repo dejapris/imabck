531c688a3af4fdd40fa734d67edfc89c54e4a5ef
frbr <fbrunet86@gmail.com>
2017-01-10 21:45:01 +0100

    init

---
+++ src/main/java/eu/ima/bck/ImabckApplication.java
@@ -0,0 +1,12 @@
+package eu.ima.bck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImabckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImabckApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/ima/bck/ImabckApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.ima.bck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImabckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
