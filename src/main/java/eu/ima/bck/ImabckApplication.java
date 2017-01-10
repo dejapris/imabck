b81a2d4852e59747e615de74f93f3446b223e0ef
frbr <fbrunet86@gmail.com>
2017-01-10 21:54:38 +0100

    ajout hello world

--- src/main/java/eu/ima/bck/ImabckApplication.java
+++ src/main/java/eu/ima/bck/ImabckApplication.java
@@ -2,11 +2,22 @@ package eu.ima.bck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 @SpringBootApplication
 public class ImabckApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImabckApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Salutations IMA!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImabckApplication.class, args);
+    }
 }
